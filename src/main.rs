62dbe1382e1e8babe1f5070dbf619849f2bbd2c2
Михаил Назаров <mikhail.a.nazarov@yandex.ru>
2024-09-29 22:29:20 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+    use std::{env, str::FromStr};
+
+    use tracing::{info, Level};
+
+    use ydb::{ydb_params, ClientBuilder, ServiceAccountCredentials};
+    #[tokio::main]
+    async fn main() -> Result<(), Box<dyn std::error::Error>> {
+        init_logs();
+    
+        let connection_string =
+            env::var("YDB_CONNECTION_STRING").map_err(|_| "YDB_CONNECTION_STRING not set")?;
+
+        let client = ClientBuilder::new_from_connection_string(connection_string)?
+            .with_credentials(ServiceAccountCredentials::from_env()?)
+            .client()?;
+
+        info!("Waiting for client");
+        client.wait().await?;
+        
+        let table_client =  client.table_client();
+
+        table_client.retry_execute_scheme_query("CREATE TABLE test (id Uint64 NOT NULL, name Utf8, age UInt8, description Utf8, PRIMARY KEY (id))").await?;
+        
+        let res = table_client.retry_transaction(|mut t| async move {
+        
+        let desc: Option<String> = None;
+        let res = t.query(ydb::Query::from(r#"
+            DECLARE $id as Uint64;
+            DECLARE $name as Text;
+            DECLARE $age as Uint8;
+            DECLARE $descr as Text?;
+
+            INSERT INTO test (id, name, age, description) VALUES ( $id, $name, $age, $descr)
+        "#).with_params(
+            ydb_params!(
+                "id" => 1,
+                "name" => "test".to_owned(),
+                "age" => 33u8,
+                "descr" => desc
+            )
+        )).await?;
+        Ok(res)   
+        }).await?;
+        info!("res: {:?}", res);
+
+        Ok(())
+    }
+
+
+    fn init_logs() {
+        let level = env::var("RUST_LOG").unwrap_or("INFO".to_string());
+        let log_level = Level::from_str(&level).unwrap();
+        let subscriber = tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(log_level)
+            .finish();
+        tracing::subscriber::set_global_default(subscriber).expect("Error setting subscriber");
+    }
