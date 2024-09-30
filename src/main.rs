1d9ef065d5fccc3db2432ffc4e1e12bef6403ec8
Mikhail Nazarov <mikhail.a.nazarov@yandex.ru>
2024-09-30 15:07:36 +0300

    111

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
     use tracing::{info, Level};
 
-    use ydb::{ydb_params, ClientBuilder, ServiceAccountCredentials};
+    use ydb::{ydb_params, ClientBuilder, Query, Row, ServiceAccountCredentials};
     #[tokio::main]
     async fn main() -> Result<(), Box<dyn std::error::Error>> {
         init_logs();
@@ -19,7 +19,7 @@
         
         let table_client =  client.table_client();
 
-        table_client.retry_execute_scheme_query("CREATE TABLE test (id Uint64 NOT NULL, name Utf8, age UInt8, description Utf8, PRIMARY KEY (id))").await?;
+        table_client.retry_execute_scheme_query("CREATE TABLE test3 (id Uint64 NOT NULL, name Utf8, age UInt8, description Utf8, PRIMARY KEY (id))").await?;
         
         let res = table_client.retry_transaction(|mut t| async move {
         
@@ -30,18 +30,40 @@
             DECLARE $age as Uint8;
             DECLARE $descr as Text?;
 
-            INSERT INTO test (id, name, age, description) VALUES ( $id, $name, $age, $descr)
+            INSERT INTO test3 (id, name, age, description) VALUES ( $id, $name, $age, $descr)
         "#).with_params(
             ydb_params!(
-                "id" => 1,
-                "name" => "test".to_owned(),
-                "age" => 33u8,
-                "descr" => desc
+                "$id" => 3u64,
+                "$name" => "test".to_owned(),
+                "$age" => 33u8,
+                "$descr" => desc
             )
         )).await?;
+        t.commit().await?;
+
         Ok(res)   
+        
         }).await?;
         info!("res: {:?}", res);
+
+        let rows: Vec<Row> = table_client
+            .retry_transaction(|mut t| async move {
+                Ok(
+                    t.query(Query::new("SELECT * FROM test3"))
+                        .await?
+                        .into_only_result()?
+                        .rows()
+                        .collect(),
+                )
+            })
+            .await?;
+    
+        for mut row in rows {
+            let id: Option<u64> = row.remove_field_by_name("id")?.try_into()?;
+            let name: Option<String> = row.remove_field_by_name("name")?.try_into()?;
+            let age: Option<u8> = row.remove_field_by_name("age")?.try_into()?;
+            println!("row id '{}' with name '{}' age: '{}'", id.unwrap(), name.unwrap(), age.unwrap());
+        }
 
         Ok(())
     }
